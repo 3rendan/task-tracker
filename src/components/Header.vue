1551a11f8ca5c543412c6555ebee646a11b71590
Brendan Ryan <brendan.ryan@pm.me>
2023-01-22 18:16:26 -0500

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+</template>
+
+<script>
+import Button from './Button.vue';
+
+export default {
+  name: 'HeaderX',
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  }
+}
+</script>
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
